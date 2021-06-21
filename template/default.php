e4f5f03839c631a2dad8031f2493b31d8e196f94
Gustavo <gustavoeraissa@hotmail.com>
2021-06-21 11:37:47 -0300

    add carousel

--- template/default.php
+++ template/default.php
@@ -46,23 +46,20 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php?p=home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?p=connection">Conectar</a>
+              <a class="nav-link active" aria-current="page" href="index.php?p=home">Inicial</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="index.php?p=disconnection">Sobre nos</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?p=connection">Contatos</a>
+            </li>
             <?php if(isset($session->username)):?>
               <li class="nav-item">
-                <a class="nav-link" href="index.php?p=disconnection">Desconectar</a>
+                <a class="nav-link" href="index.php?p=disconnection">Editar Serviços</a>
               </li>
             <li class="nav-item">
-              <a class="nav-link" href="index.php?p=edit_category">Categorias</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?p=edit_category">Serviços</a>
+              <a class="nav-link" href="index.php?p=edit_category">Editar Categorias</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="index.php?p=reset_password">
@@ -75,8 +72,45 @@
       </div>
     </nav>
 
-  <br>
-  <br>
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../img/pexels-anna-nekrashevich-6801648.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../img/pexels-daniel-dan-7624066 (1).jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../img/pexels-anna-nekrashevich-6801648.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
     <div class="container-fluid">
         <?php echo $content; ?>
