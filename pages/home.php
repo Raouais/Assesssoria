898e8e77b01e7dd623278d844e97e4f00081b921
Gustavo <gustavoeraissa@hotmail.com>
2021-06-22 14:06:36 -0300

    images

--- pages/home.php
+++ pages/home.php
@@ -47,7 +47,7 @@
 </section>
 
 <section id="services" class="bg-dark">
-    <h2 class="text-center text-primary"> <u>Serviços</u> </h2>
+    <h2 class="text-center text-primary"> Serviços </h2>
     <div class="container">
         <div class="row" style="margin-top: 20px;">
             <div class="col-md-5 bg-dark shadow  rounded">
@@ -69,32 +69,12 @@
     </div>
 </section>
 
-
-<section id="companies">
-    <div class="parallax">
-        <p class="title">Eles <u class="text-primary">confiam</u> em nós
-para a gestão de suas contas</p>
-        <ul>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-            <li>UNIRODAS</li>
-        </ul>
-    </div>
-</section>
-
-
-<section id="certificate" class="container">
+<section id="certificate" class="bg-dark">
     <h2 class="text-center text-dark"> <u>Certificado</u> </h2>
     <div>
         <div class="row shadow">
             <div class="col-md-8 rounded-left">
-                <img src="../img/Logo_Fundo_Branco.png" alt="" srcset="">
+                <img src="../img/Logo_Fundo_Branco2_prev_ui.png" alt="" srcset=""  class="img-fluid">
             </div>
             <div class="col-md-4 bg-primary rounded-right">
                 <h3 class=" text-light text-center">Certificado digital </h3>
@@ -105,42 +85,20 @@ para a gestão de suas contas</p>
     </div>
 </section>
 
-<div class="logo">
-    <div id="circle"></div>
-    <span id="letter_d">d</span>
-</div>
-
-<!-- 
-<div class="container-fluid">
-
-    <div class="present">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="../img/pexels-anna-nekrashevich-6801648.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="../img/pexels-daniel-dan-7624066 (1).jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-            <img src="../img/pexels-anna-nekrashevich-6801648.jpg" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-        </div>
-       
+<section id="companies">
+    <div class="parallax">
+        <p class="title">Eles <span class="text-primary">confiam</span> em nós
+para a gestão de suas contas</p>
+        <ul>
+            <li>UNIRODAS</li>
+            <li>GOIAS PEDRA</li>
+            <li>NEW RODAS</li>
+            <li>SPORT LIFE</li>
+            <li>MOTOCICLO</li>
+            <li>MOTOCICLO</li>
+            <li>MOTOCICLO</li>
+            <li>MOTOCICLO</li>
+            <li>MOTOCICLO</li>
+        </ul>
     </div>
-
-</div> -->
+</section>
